8735668857e4a2c03f553537cf72f002031f4a74
LaoLittle <y1941803875@outlook.com>
2022-12-16 21:58:31 +0800

    introduce managedarc for arc values

--- atri_ffi/src/lib.rs
+++ atri_ffi/src/lib.rs
@@ -1,5 +1,4 @@
 use std::mem::{ManuallyDrop, MaybeUninit};
-use std::ops::Deref;
 use std::ptr::null_mut;
 use std::{mem, slice};
 
@@ -8,145 +7,10 @@ pub mod contact;
 pub mod error;
 pub mod ffi;
 pub mod future;
+mod managed;
 pub mod message;
 pub mod plugin;
-
-#[repr(C)]
-pub struct Managed {
-    pub pointer: *mut (),
-    pub drop: extern "C" fn(*mut ()),
-}
-
-unsafe impl Send for Managed {}
-unsafe impl Sync for Managed {}
-
-impl Managed {
-    pub fn from_value<T>(value: T) -> Self {
-        let b = Box::new(value);
-        let ptr = Box::into_raw(b);
-
-        extern "C" fn _drop<B>(pointer: *mut ()) {
-            drop(unsafe { Box::from_raw(pointer.cast::<B>()) });
-        }
-
-        Self {
-            pointer: ptr.cast(),
-            drop: _drop::<T>,
-        }
-    }
-
-    pub fn from_static<T>(static_ref: &'static T) -> Self {
-        extern "C" fn _drop(_: *mut ()) {
-            // nothing to do
-        }
-
-        Self {
-            pointer: static_ref as *const _ as _,
-            drop: _drop,
-        }
-    }
-
-    pub fn as_mut_ptr(&self) -> *mut () {
-        self.pointer
-    }
-
-    pub fn as_ptr(&self) -> *const () {
-        self.pointer
-    }
-
-    /// Consume this managed value, turning it into the type
-    ///
-    /// Safety: This is unsafe because we don't know the type
-    /// behind the raw pointer
-    pub unsafe fn into_value<T>(self) -> T {
-        let ma = ManuallyDrop::new(self);
-        *Box::from_raw(ma.pointer as _)
-    }
-
-    pub unsafe fn null() -> Self {
-        extern "C" fn _drop_null(_: *mut ()) {}
-
-        Self {
-            pointer: null_mut(),
-            drop: _drop_null,
-        }
-    }
-}
-
-impl Drop for Managed {
-    fn drop(&mut self) {
-        (self.drop)(self.pointer);
-    }
-}
-
-#[repr(C)]
-pub struct ManagedCloneable {
-    pub value: Managed,
-    clone: extern "C" fn(this: *const ()) -> ManagedCloneable,
-}
-
-impl From<ManagedCloneable> for Managed {
-    fn from(ma: ManagedCloneable) -> Self {
-        ma.value
-    }
-}
-
-impl ManagedCloneable {
-    pub fn from_value<T: Clone>(value: T) -> Self {
-        extern "C" fn _clone<T: Clone>(this: *const ()) -> ManagedCloneable {
-            let this = unsafe { &*(this as *const T) };
-            ManagedCloneable::from_value(this.clone())
-        }
-
-        let value = Managed::from_value(value);
-        Self {
-            value,
-            clone: _clone::<T>,
-        }
-    }
-
-    pub unsafe fn into_value<T>(self) -> T {
-        self.value.into_value()
-    }
-
-    /// for option
-    pub unsafe fn null() -> Self {
-        extern "C" fn _clone_null(_: *const ()) -> ManagedCloneable {
-            unsafe { ManagedCloneable::null() }
-        }
-
-        Self {
-            value: Managed::null(),
-            clone: _clone_null,
-        }
-    }
-}
-
-impl Clone for ManagedCloneable {
-    fn clone(&self) -> Self {
-        (self.clone)(self.value.pointer)
-    }
-}
-
-impl Deref for ManagedCloneable {
-    type Target = Managed;
-
-    fn deref(&self) -> &Self::Target {
-        &self.value
-    }
-}
-
-pub struct ManagedRef {
-    pub pointer: *mut (),
-}
-
-impl ManagedRef {
-    pub fn from_ref<T>(val: &T) -> Self {
-        Self {
-            pointer: val as *const T as usize as *mut (),
-        }
-    }
-}
+pub use managed::*;
 
 #[repr(C)]
 pub struct RustString {
---
+++ atri_ffi/src/managed.rs
@@ -0,0 +1,185 @@
+use std::mem::ManuallyDrop;
+use std::ops::Deref;
+use std::ptr::{null, null_mut};
+use std::sync::Arc;
+
+#[repr(C)]
+pub struct Managed {
+    pub pointer: *mut (),
+    pub drop: extern "C" fn(*mut ()),
+}
+
+unsafe impl Send for Managed {}
+unsafe impl Sync for Managed {}
+
+impl Managed {
+    pub fn from_value<T>(value: T) -> Self {
+        Self::from_box(Box::new(value))
+    }
+
+    pub fn from_box<T>(b: Box<T>) -> Self {
+        extern "C" fn _drop<B>(pointer: *mut ()) {
+            drop(unsafe { Box::from_raw(pointer.cast::<B>()) });
+        }
+
+        let ptr = Box::into_raw(b);
+
+        Self {
+            pointer: ptr.cast(),
+            drop: _drop::<T>,
+        }
+    }
+
+    pub fn from_static<T>(static_ref: &'static T) -> Self {
+        extern "C" fn _drop(_: *mut ()) {
+            // nothing to do
+        }
+
+        Self {
+            pointer: (static_ref as *const T).cast_mut().cast(),
+            drop: _drop,
+        }
+    }
+
+    pub fn as_mut_ptr(&self) -> *mut () {
+        self.pointer
+    }
+
+    pub fn as_ptr(&self) -> *const () {
+        self.pointer
+    }
+
+    /// Consume this managed value, turning it into the type
+    ///
+    /// Safety: This is unsafe because we don't know the type
+    /// behind the raw pointer
+    pub unsafe fn into_value<T>(self) -> T {
+        let ma = ManuallyDrop::new(self);
+        *Box::from_raw(ma.pointer as *mut T)
+    }
+
+    pub unsafe fn null() -> Self {
+        extern "C" fn _drop_null(_: *mut ()) {}
+
+        Self {
+            pointer: null_mut(),
+            drop: _drop_null,
+        }
+    }
+}
+
+impl Drop for Managed {
+    fn drop(&mut self) {
+        (self.drop)(self.pointer);
+    }
+}
+
+#[repr(C)]
+pub struct ManagedCloneable {
+    pub value: Managed,
+    clone: extern "C" fn(this: *const ()) -> ManagedCloneable,
+}
+
+impl From<ManagedCloneable> for Managed {
+    fn from(ma: ManagedCloneable) -> Self {
+        ma.value
+    }
+}
+
+impl ManagedCloneable {
+    pub fn from_value<T: Clone>(value: T) -> Self {
+        extern "C" fn _clone<T: Clone>(this: *const ()) -> ManagedCloneable {
+            let this = unsafe { &*(this as *const T) };
+            ManagedCloneable::from_value(this.clone())
+        }
+
+        let value = Managed::from_value(value);
+        Self {
+            value,
+            clone: _clone::<T>,
+        }
+    }
+
+    /// Consume this managed value, turning it into the type
+    ///
+    /// Safety: This is unsafe because we don't know the type
+    /// behind the raw pointer
+    pub unsafe fn into_value<T>(self) -> T {
+        self.value.into_value()
+    }
+
+    /// Safety: use this as option
+    pub unsafe fn null() -> Self {
+        extern "C" fn _clone_null(_: *const ()) -> ManagedCloneable {
+            unsafe { ManagedCloneable::null() }
+        }
+
+        Self {
+            value: Managed::null(),
+            clone: _clone_null,
+        }
+    }
+}
+
+impl Clone for ManagedCloneable {
+    fn clone(&self) -> Self {
+        (self.clone)(self.value.pointer)
+    }
+}
+
+impl Deref for ManagedCloneable {
+    type Target = Managed;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+pub struct ManagedArc {
+    pub pointer: *const (),
+    pub drop: extern "C" fn(*const ()),
+}
+
+impl ManagedArc {
+    pub fn from_arc<T>(arc: Arc<T>) -> Self {
+        extern "C" fn _drop<B>(pointer: *const ()) {
+            drop(unsafe { Arc::from_raw(pointer.cast::<B>()) });
+        }
+
+        let ptr = Arc::into_raw(arc);
+
+        Self {
+            pointer: ptr.cast(),
+            drop: _drop::<T>,
+        }
+    }
+
+    /// Consume this managed arc value, turning it into arc value
+    ///
+    /// Safety: This is unsafe because we don't know the type
+    /// behind the raw pointer
+    pub unsafe fn into_arc<T>(self) -> Arc<T> {
+        let ma = ManuallyDrop::new(self);
+        Arc::<T>::from_raw(ma.pointer.cast())
+    }
+
+    pub fn as_ptr(&self) -> *const () {
+        self.pointer
+    }
+
+    /// Safety: use this as option
+    pub unsafe fn null() -> Self {
+        extern "C" fn _drop_null(_: *const ()) {}
+
+        Self {
+            pointer: null(),
+            drop: _drop_null,
+        }
+    }
+}
+
+impl Drop for ManagedArc {
+    fn drop(&mut self) {
+        (self.drop)(self.pointer);
+    }
+}
